7e5c94ab55f77437143ae5411a07afe9538bf09e
DedaloIcarus <davinci_learning@protonmail.com>
2024-12-29 13:05:49 +0100

    add github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
     images: {
         unoptimized: true,
-    }
+    },
+    assetPrefix: isProd ? '/actividad2_tutorial_nextjs/' : '',
+    basePath: isProd ? '/actividad2_tutorial_nextjs' : '',
 };
 
 export default nextConfig;
