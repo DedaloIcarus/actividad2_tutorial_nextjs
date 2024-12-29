a8dd2ebfd08d3be33975d23466158119701a907a
DedaloIcarus <davinci_learning@protonmail.com>
2024-12-29 20:45:55 +0100

    section 6 - 7

--- src/app/6-c/page.js
+++ src/app/6-c/page.js
@@ -1,10 +1,20 @@
 import navitems from "@/config/navitems";
 
 export default function Page() {
-  return (
-      <div>
-          <h1>{navitems[5].title}</h1>
-          <h5>{navitems[5].items[2].title}</h5>
-      </div>
-  );
+    return (
+        <div className="text-center p-10">
+            <div className="flex flex-col gap-2 mb-10">
+                <h1 className="text-3xl">6 - {navitems[5].title}</h1>
+                <h5 className="text-xl">c - {navitems[5].items[2].title}</h5>
+            </div>
+            <div className="text-start">
+                <p>
+                    Para referenciar ficheros creados desde otras aplicaciones, una de las
+                    mejores formas consiste en empaquetar esos componentes como un
+                    paquete NPM para posteriormente instalarlo en la aplicación que uno
+                    quiera.
+                </p>
+            </div>
+        </div>
+    );
 }
