638a587f156d3a861559802204fc2623aec6842f
DedaloIcarus <davinci_learning@protonmail.com>
2024-12-29 01:01:17 +0100

    configure sidebar in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AppSidebar from "@/components/ui/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppSidebar>
+          {children}
+        </AppSidebar>
       </body>
     </html>
   );
--- src/components/ui/app-sidebar.jsx
+++ src/components/ui/app-sidebar.jsx
@@ -123,7 +123,7 @@ const data = {
     ],
 }
 
-export default function AppSidebar() {
+export default function AppSidebar({ children }) {
     return (
         <SidebarProvider>
             <Sidebar collapsible="icon">
@@ -175,12 +175,9 @@ export default function AppSidebar() {
                     </div>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                        <div className="aspect-video rounded-xl bg-muted/50" />
+                    <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+                        {children}
                     </div>
-                    <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
                 </div>
             </SidebarInset>
         </SidebarProvider>
